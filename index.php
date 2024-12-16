e0057cead64a6208364ae056f6cef66d47156781
jok <fghj@dfgh.com>
2024-12-16 18:01:35 +0530

    123

--- index.php
+++ index.php
@@ -1344,6 +1344,7 @@ include 'header.php';
         </div>
     </div>
 </div> -->
+<?php include 'reel-popup.php'; ?>
 <div class="tagembed-widget" style="width:100%;height:100%" data-widget-id="2143236" data-tags="false" view-url="https://widget.tagembed.com/2143236"></div>
 <script src="https://widget.tagembed.com/embed.min.js" type="text/javascript"></script>
 <!-- Galleary section ends here -->
---
+++ reel-popup.php
@@ -0,0 +1,15 @@
+
+
+    <div class="popup" style="position: fixed;
+  bottom: 10px;
+  right: 10px;
+  width: 150px;
+  height: 250px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: slider 1s ease-in; z-index:10;">
+        <!-- link will random from database -->
+        <video src="https://video.twimg.com/amplify_video/1868449912918261760/vid/avc1/720x1280/JjRMg47-yQHCKW9e.mp4" style="height: 100%;
+  width: 100%;" frameborder="0" class="popup-video" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" autoplay="" loop="" muted="" playsinline=""></video>
+    </div>
